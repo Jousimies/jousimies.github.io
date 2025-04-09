59a3fdc6cff0b741d7cbd245e60ed708a99e97e6
Jousimies <Jousimies@users.noreply.github.com>
2025-04-09 06:43:16 +0000

    deploy: Jousimies/blog-source@8957dd1b332b4f82f9904194541c42f9a7b1f23e

--- static/js/main.js
+++ static/js/main.js
@@ -7,11 +7,13 @@ function addNavigationButtons() {
   // 创建返回上一页按钮
   const backButton = document.createElement('button');
   backButton.innerHTML = '← 返回上一页';
+  backButton.classList.add('back-button');
   backButton.onclick = () => window.history.back();
 
   // 创建返回首页按钮
   const homeButton = document.createElement('button');
   homeButton.innerHTML = '🏠 返回首页';
+  homeButton.classList.add('home-button');
   homeButton.onclick = () => window.location.href = 'https://jousimies.github.io';
 
   // 添加按钮到容器
@@ -24,3 +26,16 @@ function addNavigationButtons() {
 
 // 等页面加载完后，添加导航按钮
 window.onload = addNavigationButtons;
+
+// main.js
+// 加载highlight.js并高亮代码块
+// document.addEventListener("DOMContentLoaded", function() {
+//   // 初始化代码高亮
+//   hljs.highlightAll();
+// });
+
+// main.js
+// 加载prism.js并高亮代码块
+document.addEventListener("DOMContentLoaded", function() {
+  Prism.highlightAll();
+});
